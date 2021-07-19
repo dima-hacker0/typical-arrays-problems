03394419cdf341c0e869858bd92a1ab5175ebe9d
Dima Vaitsiachovski <boretskiy70@bk.ru>
2021-07-20 00:05:58 +0300

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array) {
+    return 0
+  }
+  if(array.length == 0) return 0;
+  let min = array[0];
+  for(let i = 0; i < array.length - 1; i++) {
+    if(array[i + 1] < min) min = array[i + 1];
+  } 
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array) {
+    return 0
+  }
+  if(array.length == 0) return 0;
+  let max = array[0];
+  for(let i = 0; i < array.length - 1; i++) {
+    if(array[i + 1] > max) max = array[i + 1];
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array) {
+    return 0
+  }
+  if(array.length == 0) return 0;
+  let sum = 0;
+  for(let i = 0; i < array.length; i++)
+  sum += array[i];
+  sum /= array.length;
+  return sum;
 }
